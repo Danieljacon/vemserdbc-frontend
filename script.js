774ca43fd1cc59274d3f94c96a14a0fc36e5d135
Danieljacon <danielcamillo2020@gmail.com>
2022-09-26 23:24:41 -0300

    Adicionada a função de criar post

--- script.js
+++ script.js
@@ -2,23 +2,22 @@ const contentPost = document.querySelector(".content");
 const url = "https://jsonplaceholder.typicode.com/albums/1/photos";
 let data;
 
-const createPost = () => {
-
-}
+const createPost = (post) => {
+  const postElement = document.createElement("div");
+  postElement.classList.add("post");
+  postElement.innerHTML = `
+              <img src="${post.url}" alt="" class="post-image">
+              <p class="post-text">${post.title}</p>
+          `;
+  contentPost.appendChild(postElement);
+};
 
 const getPosts = async () => {
   try {
     const response = await fetch(url);
     data = await response.json();
-
     data.forEach((post) => {
-      const postElement = document.createElement("div");
-      postElement.classList.add("post");
-      postElement.innerHTML = `
-                <img src="${post.url}" alt="" class="post-image">
-                <p class="post-text">${post.title}</p>
-            `;
-      contentPost.appendChild(postElement);
+      createPost(post);
     });
   } catch (error) {
     console.log(error);
